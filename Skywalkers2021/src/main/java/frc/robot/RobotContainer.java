80e17a32e902341871621fa04c42eb99df8f1b7c
Karthik Subramanian <88288818+KarthikSubramanian07@users.noreply.github.com>
2022-02-11 19:34:13 -0800

    Update RobotContainer.java

--- Skywalkers2021/src/main/java/frc/robot/RobotContainer.java
+++ Skywalkers2021/src/main/java/frc/robot/RobotContainer.java
@@ -1,90 +1,34 @@
-/*----------------------------------------------------------------------------*/
-/* Copyright (c) 2018-2019 FIRST. All Rights Reserved.                        */
-/* Open Source Software - may be modified and shared by FRC teams. The code   */
-/* must be accompanied by the FIRST BSD license file in the root directory of */
-/* the project.                                                               */
-/*----------------------------------------------------------------------------*/
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
 
 package frc.robot;
 
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.XboxController;
-import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.button.JoystickButton;
-import frc.robot.commands.AutonomousOne;
-import frc.robot.commands.ToggleQuickTurn;
-import frc.robot.subsystems.Climber;
-import frc.robot.subsystems.Drivetrain;
 import frc.robot.subsystems.Intake;
-import frc.robot.subsystems.Shooter;
-import frc.robot.subsystems.Spinner;
-import frc.robot.subsystems.Transfer;
 
 /**
- * This class is where the bulk of the robot should be declared.  Since Command-based is a
+ * This class is where the bulk of the robot should be declared. Since Command-based is a
  * "declarative" paradigm, very little robot logic should actually be handled in the {@link Robot}
- * periodic methods (other than the scheduler calls).  Instead, the structure of the robot
- * (including subsystems, commands, and button mappings) should be declared here.
+ * periodic methods (other than the scheduler calls). Instead, the structure of the robot (including
+ * subsystems, commands, and button mappings) should be declared here.
  */
 public class RobotContainer {
   // The robot's subsystems and commands are defined here...
-
-
-  public static XboxController driverJoystick;
-  private final Drivetrain drivetrain;
   private final Intake intake;
-  private final Transfer transfer;
-  private final Shooter shooter;
-  private final Climber climber;
-  private final Spinner spinner;
 
-  private final AutonomousOne autonomousOne;
-
-
-
-  /**
-   * The container for the robot.  Contains subsystems, OI devices, and commands.
-   */
+  /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
-
-    drivetrain = new Drivetrain();
-    intake = new Intake();
-    transfer = new Transfer();
-    shooter = new Shooter();
-    climber = new Climber();
-    spinner = new Spinner();
-
-    autonomousOne = new AutonomousOne();
-
-    driverJoystick = new XboxController(Constants.JOYSTICK_NUMBER);
-
-
-
     // Configure the button bindings
-    configureButtonBindings();
+    intake = new Intake();
   }
 
   /**
-   * Use this method to define your button->command mappings.  Buttons can be created by
+   * Use this method to define your button->command mappings. Buttons can be created by
    * instantiating a {@link GenericHID} or one of its subclasses ({@link
-   * edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then passing it to a
-   * {@link edu.wpi.first.wpilibj2.command.button.JoystickButton}.
+   * edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then passing it to a {@link
+   * edu.wpi.first.wpilibj2.command.button.JoystickButton}.
    */
-  private void configureButtonBindings() {
-
-    JoystickButton aButton = new JoystickButton(driverJoystick, XboxController.Button.kA.value);
-    aButton.whenPressed(new ToggleQuickTurn(drivetrain));
-    
+  private void configureButtonBindings() {}
   }
-
-
-  /**
-   * Use this to pass the autonomous command to the main {@link Robot} class.
-   *
-   * @return the command to run in autonomous
-   */
-  public Command getAutonomousCommand() {
-    return autonomousOne;
-
-  }
-}
